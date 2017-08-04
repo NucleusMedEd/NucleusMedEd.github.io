2b743471039cd199c7ce8a2097c9faad2cdda13b
NucleusMedEd <shak.ahme@gmail.com>
2017-08-04 16:45:26 -0400

    Update nucleusmeded.js

--- nucleusmeded.js
+++ nucleusmeded.js
@@ -1,4 +1,7 @@
-// this inserts BS modal to ankiweb id=f1 which is the second form box 
-var factModal = '<br><button type="button" class="btn btn-default btn-md" data-toggle="modal" data-target="#myModal">insert name</button><div id="myModal" class="modal fade" role="dialog"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><h4 class="modal-title">Modal Header</h4><button type="button" class="close" data-dismiss="modal">&times;</button></div><div class="modal-body"><p>Some text in the modal.</p></div><div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal">Close</button></div></div></div></div>';    
+	// this inserts BS modal to ankiweb id=f1 which is the second form box 
+var bottomLineModal = '<br><div><button type="button" class="btn btn-default btn-md" data-toggle="modal" data-target="#bottomLine">Bottom Line</button><div id="bottomLine" class="modal fade" role="dialog" style="display: none;" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><h4 class="modal-title">name</h4><button type="button" class="close" data-dismiss="modal">×</button></div><div class="modal-body"><p>insert text here...</p></div><div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal">Close</button></div></div></div></div></div>';  
+
+var explanationModal = '<br><div><button type="button" class="btn btn-default btn-md" data-toggle="modal" data-target="#explanation">Explanation</button><div id="explanation" class="modal fade" role="dialog" style="display: none;" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><h4 class="modal-title">name</h4><button type="button" class="close" data-dismiss="modal">×</button></div><div class="modal-body"><p>insert text here...</p></div><div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal">Close</button></div></div></div></div></div>';
     
-    $('[id=f1]').append(factModal);
+    $('[id=f1]').append(explanationModal);
+    $('[id=f1]').append(bottomLineModal);
