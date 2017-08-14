f0f28dd58e7469cfd95a9d478447ad93374a15e9
NucleusMedEd <shak.ahme@gmail.com>
2017-08-14 18:14:22 -0400

    Create nucleusindex.js

---
+++ js/nucleusindex.js
@@ -0,0 +1,11 @@
+var RA_define = 'RA is a systemic autoimmune disorder characterized by chronic, destructive, inflammatory arthritis with symmetric involvement of both large and small joints that results in synovial hypertrophy and pannus formation ultimately leading to erosion of adjacent cartilage, bone and tendons.' 
+
+var RA_risk_factors = 'Risk factors include female gender, age 35-40 and HLA-DR4.'
+
+var RA_presentation = 'It presents with insidious onset of morning stiffness lasting more than an hour, warm and swollen joints and systemic symptoms such as fever and fatigue.'
+
+var RA_labs = 'IgM antibodies against the Fc portion of IgG, also known as rheumatoid factor may be used as a screening tool along with ANA and ESR. The most specific lab for RA is anti-cyclic citrullinated peptide (anti-CCP).'
+
+var RA_treatment = 'RA can be treated with NSAID’s, hydroxychloroquine, sulfasalazine, methotrexate, rituximab, etanercept, infliximab or adalimumab. Etanercept is a recombinant form of the human tumor necrosis factor receptor that binds to and inactivates soluble tumor necrosis factor. Etanercept and infliximab may predispose patients to reactivation of a latent TB infection.'
+
+var Rheumatoid_arthritis = [RA_define, RA_risk_factors, RA_presentation, RA_labs, RA_treatment];
