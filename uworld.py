83417dd925e89f284e6ce8c1f4f476ad6b56edd4
NucleusMedEd <shak.ahme@gmail.com>
2017-09-06 17:25:17 -0400

    Update uworld.py

--- uworld.py
+++ uworld.py
@@ -1,4 +1,4 @@
-Gastrointestinal_Nutrition = {}
+Gastrointestinal_Nutrition = {'name':'Gastrointestinal & Nutrition'}
   ['Total parenteral nutrition']
   ['Spontaneous bacterial peritonitis']
   ['Esophageal rupture']
@@ -7,54 +7,54 @@ Gastrointestinal_Nutrition = {}
   ['Cholelithiasis']
   ['Hepatic Encephalopathy']
   
-CardiovascularSystem = {}
+CardiovascularSystem = {'name':'Cardiovascular System'}
   ['Syncope']
   ['Cardiac tamponade']
   ['Transient ischemic attack']
   ['Ventricular tachycardia']
   ['Aortic stenosis']
 
-Endocrine_Diabetes_Metabolism = {}
-  ['Toxic alcohol']
+Endocrine_Diabetes_Metabolism = {'name':'Endocrine, Diabetes, & Metabolism'}
+  ['Toxic alcohol'] = [AlcoholKetoacidosis, Methanol, EthyleneGlycol, IsopropylAlcohol]
   ['Cushing syndrome']
   ['Hyperaldosteronism']
   ['Hypercalcemia']
   ['Hyperparathyroidism']
 
-Pregnancy_Childbirth_Puerperium = {}
+Pregnancy_Childbirth_Puerperium = {'name':'Pregnancy, Childbirth & Puerperium'}
   ['Fetal heart tracing']
-  ['Labor management']
+  ['Labor management'] = [Protraction, Arrest]
   ['Neural tube defects']
   ['Gestational diabetes']
 
-Rheumatology_Orthopedics_Sports = {}
+Rheumatology_Orthopedics_Sports = {'name':'Rheumatology/Orthopedics & Sports'}
   ['Pseudomonas Aeruginosa']
   ['Spinal stenosis']
   ['Polymyositis']
   ['Dermatomyositis']
   ['SLE']
 
-NervousSystem = {}
+NervousSystem = {'name':'Nervous System'}
   ['Herpes simplex encephalitis']
   ['Brain tumors']
   ['Amyotrophic lateral sclerosis']
   ['Seizures']
 
-Pulmonary_CriticalCare = {}
+Pulmonary_CriticalCare = {'name':'Pulmonary & Critical Care'}
   ['Pulmonary fibrosis']
   ['Pleural effusion']
   ['Superior vena cava syndrome']
   ['Aspiration pneumonia']
   ['Pneumothorax']
 
-Allergy_Immunology = {}
-  ['HIV']
+Allergy_Immunology = {'name':'Allergy & Immunology'}
+  ['HIV'] = [Vaccines_forHIV]
 
-Dermatology = {}
-  ['Bullous pemphigoid']
+Dermatology = {'name':'Dermatology'}
+  ['Bullous pemphigoid'] = [riskFactor, presentation, workup, treatment, association]
   ['Pemphigus vulgaris']
 
-Renal_UrinarySystem_Electrolytes = {}
+Renal_UrinarySystem_Electrolytes = {'name':'Renal, Urinary Systems & Electrolytes'}
   ['IgA nephropathy']
   ['Urinary incontinence']
 
