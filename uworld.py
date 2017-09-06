112a12609e8b80050dd3875355eba62fdb1ea555
NucleusMedEd <shak.ahme@gmail.com>
2017-09-06 16:52:14 -0400

    Update uworld.py

--- uworld.py
+++ uworld.py
@@ -1,1 +1,61 @@
+Gastrointestinal_Nutrition = {}
+  ['Total parenteral nutrition']
+  ['Spontaneous bacterial peritonitis']
+  ['Esophageal rupture']
+  ['Chronic pancreatitis']
+  ['Peripheral vascular disease']
+  ['Cholelithiasis']
+  ['Hepatic Encephalopathy']
+  
+CardiovascularSystem = {}
+  ['Syncope']
+  ['Cardiac tamponade']
+  ['Transient ischemic attack']
+  ['Ventricular tachycardia']
+  ['Aortic stenosis']
+
+Endocrine_Diabetes_Metabolism = {}
+  ['Toxic alcohol']
+  ['Cushing syndrome']
+  ['Hyperaldosteronism']
+  ['Hypercalcemia']
+  ['Hyperparathyroidism']
+
+Pregnancy_Childbirth_Puerperium = {}
+  ['Fetal heart tracing']
+  ['Labor management']
+  ['Neural tube defects']
+  ['Gestational diabetes']
+
+Rheumatology_Orthopedics_Sports = {}
+  ['Pseudomonas Aeruginosa']
+  ['Spinal stenosis']
+  ['Polymyositis']
+  ['Dermatomyositis']
+  ['SLE']
+
+NervousSystem = {}
+  ['Herpes simplex encephalitis']
+  ['Brain tumors']
+  ['Amyotrophic lateral sclerosis']
+  ['Seizures']
+
+Pulmonary_CriticalCare = {}
+  ['Pulmonary fibrosis']
+  ['Pleural effusion']
+  ['Superior vena cava syndrome']
+  ['Aspiration pneumonia']
+  ['Pneumothorax']
+
+Allergy_Immunology = {}
+  ['HIV']
+
+Dermatology = {}
+  ['Bullous pemphigoid']
+  ['Pemphigus vulgaris']
+
+Renal_UrinarySystem_Electrolytes = {}
+  ['IgA nephropathy']
+  ['Urinary incontinence']
+
 
