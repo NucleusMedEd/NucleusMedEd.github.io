25211c909cadab84904a2cdc318c1d3fad414618
NucleusMedEd <shak.ahme@gmail.com>
2017-11-04 03:02:47 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,53 @@
+
+$("#user_input").keyup(function(event) {
+  if (event.keyCode === 13) {
+      button.mousePressed(chat);
+  }
+});
+
+
+function setup() {                          //  FORM
+  // put setup code here
+  noCanvas();
+
+  let bot = new RiveScript();
+  bot.loadFile("nucleus.rive", nucleusReady, nucleusError);
+  function nucleusReady(){
+    console.log('chatbot ready!')
+    bot.sortReplies();
+  }
+  function nucleusError(){
+    console.log('chatbot error?!')
+  }
+  let button = select('#send');
+  let user_input = select('#user_input');
+  let output = select('#output');
+
+  button.mousePressed(chat);
+
+
+  function chat() {
+    let input = user_input.value();
+    let reply = bot.reply("local-user", input);
+    output.html(reply);
+  }
+  //button = createButton('Key Codes');
+  //button.mousePressed(keyCodes);
+}
+function keyCodes() {
+  keyCodeDiv = document.getElementById('keycode');
+  if (keyCodeDiv.style.display === "block") {
+    keyCodeDiv.style.display = "none";
+  } else {
+    keyCodeDiv.style.display = "block";
+  }
+}
+function changeStyle(){
+  //txt.style("background-color", "pink");
+  //txt.style("padding", "24px");
+}
+
+
+function draw() {                           //  FUNCTION
+
+}
