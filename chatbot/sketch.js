efa648d7e2ad5fed0d352e134b2417752afa1586
NucleusMedEd <shak.ahme@gmail.com>
2017-11-09 02:50:32 -0500

    Add files via upload

--- chatbot/sketch.js
+++ chatbot/sketch.js
@@ -1,29 +1,39 @@
-function setup() {
+function setup() {                          //  FORM
+
   noCanvas();
-
+// attemp infinity
   let bot = new RiveScript();
-  bot.loadFile("brain/disorders.rive", brainReady, brainError)
+  bot.loadFile([
+          "brain/disorders.rive",
+          "brain/drugs.rive",
+          "brain/muscles.rive",
+          "brain/testsuite.rive",
+          "brain/rs-standard.rive"
+  ], brainReady, brainError);
 
-  function brainReady() {
-    console.log('Chatbot ready!')
+  function brainReady(){
+    console.log('chatbot ready!')
     bot.sortReplies();
   }
 
-  function brainError() {
-    console.log('Chatbot Error!')
+  function brainError(){
+    console.log('chatbot error?!')
   }
 
-  let button = select('#submitNow');
+  let button = select('#send');
   let user_input = select('#user_input');
   let output = select('#output');
 
   button.mousePressed(chat);
+  // test console.log(output);
 
   function chat() {
     let input = user_input.value();
+    console.log(input);
     let reply = bot.reply("local-user", input);
-
     output.html(reply);
+
+    console.log(reply);
   }
 
 }
