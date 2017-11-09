f70779525869274c1f909cf393086660677b44b4
NucleusMedEd <shak.ahme@gmail.com>
2017-11-09 02:29:20 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,53 +1,39 @@
-
-/* .   use enter on keypress?? dontknow
-$("#user_input").keyup(function(event) {
-  if (event.keyCode === 13) {
-      button.mousePressed(chat);
-  }
-});
-*/
-
 function setup() {                          //  FORM
-  // put setup code here
+
   noCanvas();
 // attemp infinity
   let bot = new RiveScript();
-  bot.loadFile("brain/disorders.rive", brainReady, brainError);
+  bot.loadFile([
+          "brain/disorders.rive",
+          "brain/drugs.rive",
+          "brain/muscles.rive",
+          "brain/testsuite.rive",
+          "brain/rs-standard.rive"
+  ], brainReady, brainError);
+
   function brainReady(){
     console.log('chatbot ready!')
     bot.sortReplies();
   }
+
   function brainError(){
     console.log('chatbot error?!')
   }
+
   let button = select('#send');
   let user_input = select('#user_input');
   let output = select('#output');
 
   button.mousePressed(chat);
-
+  // test console.log(output);
 
   function chat() {
     let input = user_input.value();
+    console.log(input);
     let reply = bot.reply("local-user", input);
     output.html(reply);
-  }
-  //button = createButton('Key Codes');
-  //button.mousePressed(keyCodes);
-}
-/*
-function keyCodes() {
-  keyCodeDiv = document.getElementById('keycode');
-  if (keyCodeDiv.style.display === "block") {
-    keyCodeDiv.style.display = "none";
-  } else {
-    keyCodeDiv.style.display = "block";
-  }
-}
-function changeStyle(){
-  //txt.style("background-color", "pink");
-  //txt.style("padding", "24px");
-} 
-*/
 
+    console.log(reply);
+  }
 
+}
